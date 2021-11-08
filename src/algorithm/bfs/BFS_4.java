311b242935411a758abb7ddf6d42c601f74d944e
phehe <haeunoo@nate.com>
2021-11-08 20:43:07 +0900

    2021/11/08

---
+++ src/algorithm/bfs/BFS_4.java
@@ -0,0 +1,53 @@
+package algorithm.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node_2 {
+    int data;
+    Node lt, rt;
+
+    public Node_2(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
+
+public class BFS_4 {
+    Node root;
+
+    public int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+
+        int L = 0;
+
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                if (cur.lt == null && cur.rt == null) {
+                    return L;
+                }
+                if (cur.lt != null) {
+                    Q.offer(cur.lt);
+                }
+                if (cur.rt != null) {
+                    Q.offer(cur.rt);
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        BFS_4 tree = new BFS_4();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.BFS(tree.root));
+    }
+}
