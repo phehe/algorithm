7e12d713952c0b43523c3d8db0ef4fc38e9c2fe4
phehe <haeunoo@nate.com>
2021-11-27 13:40:46 +0900

    2021/11/27

---
+++ src/algorithm/dfs/DFS_9.java
@@ -0,0 +1,34 @@
+package algorithm.dfs;
+
+import java.util.Scanner;
+
+public class DFS_9 {
+    static int answer = Integer.MIN_VALUE, c, n;
+
+    public void DFS(int L, int sum, int[] arr) {
+        if (sum > c) {
+            return;
+        }
+        if (L == n) {
+            answer = Math.max(answer, sum);
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        c = sc.nextInt();
+        n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        DFS_9 T = new DFS_9();
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
