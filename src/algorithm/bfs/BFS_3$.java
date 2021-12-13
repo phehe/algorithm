e97f1d29c73fbc46cc580312a2314f7320d9a3df
phehe <haeunoo@nate.com>
2021-12-13 17:32:55 +0900

    2021/12/13

---
+++ src/algorithm/bfs/BFS_3$.java
@@ -0,0 +1,56 @@
+package algorithm.bfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class BFS_3$ {
+    static int n, m, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch, dis;
+
+    public void BFS(int v) {
+        ch[v] = 1;
+        dis[v] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        while (!queue.isEmpty()) {
+            int cv = queue.poll();
+            for (int nv : graph.get(cv)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    queue.offer(nv);
+                    dis[nv] = dis[cv] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        graph = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n + 1];
+        dis = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+
+        BFS_3$ bf = new BFS_3$();
+        bf.BFS(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " : " + dis[i]);
+        }
+    }
+}
